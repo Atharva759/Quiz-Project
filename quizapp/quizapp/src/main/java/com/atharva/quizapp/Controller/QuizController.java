f15c61a36df1e46f2b20c43bf4cbfce7e72c1ed1
Atharva <atharvakhadake759@gmail.com>
2025-09-02 17:04:42 +0530

    Initial commit

---
+++ quizapp/quizapp/src/main/java/com/atharva/quizapp/Controller/QuizController.java
@@ -0,0 +1,31 @@
+package com.atharva.quizapp.Controller;
+
+
+
+import com.atharva.quizapp.Entity.Question;
+import com.atharva.quizapp.Entity.QuestionWrapper;
+import com.atharva.quizapp.Service.QuizService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/quiz")
+public class QuizController {
+
+    @Autowired
+    QuizService quizService;
+
+    @PostMapping("/create")
+    public ResponseEntity<String> createQuiz(@RequestParam String category,@RequestParam int numQ, @RequestParam String title){
+        return quizService.createQuiz(category,numQ,title);
+    }
+
+    @GetMapping("/get/{id}")
+    public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(@PathVariable int id){
+        return quizService.getQuizQuestions(id);
+    }
+}
